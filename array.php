8c91a6a925f9508fdf553ae7a80180d5bd33991a
Sergey <seregandreev6@gmail.com>
2021-12-09 20:03:58 +0300

    ыфвавыпавптарь

--- array.php
+++ array.php
@@ -15,4 +15,76 @@
 
     echo '<pre>';
     print_r($person);
+
+   echo '<hr>';
+
+   $colors = ['red', 'green', 'blue'];
+   $output = '';
+   foreach ($colors as $color) {
+       $output = $output . ' '. $color;
+   }
+
+   $str_colors = implode(', ', $colors);
+   echo "$str_colors<br>";
+
+   $new_str = "Предложение из 4 слов";
+   $arr_str = explode(' ', $new_str);
+
+   print_r($arr_str);
+
+   $numbers = [2, 4, 6, 10, 11, 5];
+
+   sort($numbers);
+   print_r($numbers);
+
+   asort($person);
+   print_r($person);
+
+   ksort($person);
+   print_r($person);
+
+   krsort($person);
+   print_r($person);
+
+   echo '<hr>';
+
+   $persons = [
+       [
+        'name' => 'Sergey',
+        'age'  => 31,
+        'city' => 'New York'
+       ],
+       [
+        'name' => 'Ivan',
+        'age'  => 30,
+        'city' => 'MGN'
+       ],
+       [
+        'name' => 'Petr',
+        'age'  => 33,
+        'city' => 'Kazan'
+       ]
+    ];
+
+    function sortByAge($a, $b) {
+        if ($a['age'] > $b['age']) {
+            return 1;
+        } else if ($a['age'] < $b['age']) {
+            return -1;
+        } else {
+            return 0;
+        }
+    } 
+
+    print_r($persons);
+
+    usort($persons, 'sortByAge');
+    print_r($persons);
+
+    function sortByName($a, $b) {
+        return $a['name'] <=> $b['name'];
+    }
+
+    usort($persons, 'sortByName');
+    print_r($persons)
 ?>
--- index.php
+++ index.php
@@ -6,5 +6,13 @@
     //include_once 'vars.php';
     //include_once 'conditions.php';
     //include_once 'types.php';
+<<<<<<< Updated upstream
+<<<<<<< Updated upstream
     include_once 'loop.php';
+=======
+    include_once 'array.php';
+>>>>>>> Stashed changes
+=======
+    include_once 'array.php';
+>>>>>>> Stashed changes
 ?>
